66f8a478746b3b97883c9857e9caa58fc5962639
Nicklaus Roach <roachnt@miamioh.edu>
2017-08-09 17:27:18 +0000

    Complete family rules

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -17,7 +17,7 @@ from production import IF, AND, OR, NOT, THEN, forward_chain
 #    2. the consequent
 #    3. both
 
-ANSWER_1 = 'your answer here'
+ANSWER_1 = 'the consequent'
 
 # A rule-based system about Monty Python's "Dead Parrot" sketch
 # uses the following rules:
@@ -37,10 +37,10 @@ ANSWER_1 = 'your answer here'
 
 # Will this system produce the datum 'Polly is pining for the
 # fjords'?  Answer 'yes' or 'no'.
-ANSWER_2 = 'your answer here'
+ANSWER_2 = 'no'
 
 # Which rule contains a programming error? Answer '1' or '2'.
-ANSWER_3 = 'your answer here'
+ANSWER_3 = '2'
 
 # If you're uncertain of these answers, look in tests.py for an
 # explanation.
@@ -69,11 +69,11 @@ ANSWER_3 = 'your answer here'
 # what is asked.  After we start the system running, which rule
 # fires first?
 
-ANSWER_4 = 'your answer here'
+ANSWER_4 = '1'
 
 # Which rule fires second?
 
-ANSWER_5 = 'your answer here'
+ANSWER_5 = '0'
 
 
 # Problem 1.3.1: Poker hands
@@ -114,7 +114,17 @@ TEST_RESULTS_TRANS2 = forward_chain([transitive_rule],
 
 # Then, put them together into a list in order, and call it
 # family_rules.
-family_rules = [ ]                    # fill me in
+family_rules = [
+    IF(OR('male (?x)', 'female (?x)'), THEN('same-identity (?x) (?x)')),
+    IF(AND('parent (?x) (?y)', 'parent (?x) (?z)', 'male (?y)', NOT('same-identity (?y) (?z)')), THEN('brother (?y) (?z)')), 
+    IF(AND('parent (?x) (?y)', 'parent (?x) (?z)', 'female (?y)', NOT('same-identity (?y) (?z)')), THEN('sister (?y) (?z)')),
+    IF(AND('parent (?x) (?y)', 'female (?x)'), THEN('mother (?x) (?y)')),
+    IF(AND('parent (?x) (?y)', 'male (?x)'), THEN('father (?x) (?y)')),
+    IF(AND('parent (?x) (?y)', 'male (?y)'), THEN('son (?y) (?x)')),
+    IF(AND('parent (?x) (?y)', 'female (?y)'), THEN('daughter (?y) (?x)')),
+    IF(AND(OR('brother (?x) (?y)', 'sister (?x) (?y)'), 'parent (?x) (?z)', 'parent (?y) (?w)'), NOT('same-identity (?x) (?y)'), THEN('cousin (?z) (?w)')),
+    IF(AND('parent (?x) (?y)', 'parent (?y) (?z)'), THEN('grandparent (?x) (?z)')),
+    IF('grandparent (?x) (?y)', THEN('grandchild (?y) (?x)'))]                    # fill me in
 
 # Some examples to try it on:
 # Note: These are used for testing, so DO NOT CHANGE
